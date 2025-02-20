5efbc70e7f0448da2d59af8542d948ac26aedd69
dhruv <dhruvcomputers11@gmail.com>
2025-02-20 11:28:33 +0530

    new npm i

--- src/components/ecommerce/charts/Charts.tsx
+++ src/components/ecommerce/charts/Charts.tsx
@@ -49,6 +49,7 @@ const Charts = () => {
                     </h3>
                 </AccordionSummary>
 
+
                 <AccordionDetails>
                     <div className="grid grid-cols-12 gap-4 md:gap-5 pb-6">
                         <div className="col-span-12  space-y-6 xl:col-span-10">
